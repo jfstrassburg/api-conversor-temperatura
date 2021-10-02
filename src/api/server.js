d0554c51fd3f43592f4bbdd54f01db370072aa0b
Francisco Chaves <francisco.jschaves@gmail.com>
2021-10-02 14:18:56 -0300

    feat: Initial swagger setup

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
+
 const routerConverter = require('./routes/converter.routes');
 
 app.use(cors());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 routerConverter(app);
 
